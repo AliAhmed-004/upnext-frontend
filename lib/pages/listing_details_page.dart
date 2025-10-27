064f1557f016c8aee19530ff57af347ca8d82068
AliAhmed-004 <eliehmed908@gmail.com>
2025-10-27 22:10:04 +0500

    Made item details page better

--- lib/pages/listing_details_page.dart
+++ lib/pages/listing_details_page.dart
@@ -3,8 +3,12 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:upnext/components/item_location_map.dart';
 
 import '../env.dart';
+import '../models/listing_model.dart';
 
 class ListingDetailsPage extends StatefulWidget {
   final String listingId;
@@ -17,9 +21,13 @@ class ListingDetailsPage extends StatefulWidget {
 class _ListingDetailsPageState extends State<ListingDetailsPage> {
   late final String listingId;
 
-  String _userName = "Loading...";
+  String _createdBy = "Loading...";
   String _title = "Loading...";
   String _description = "Loading...";
+  String _category = "Loading...";
+  String _formattedDate = "Loading...";
+  String _status = "Loading...";
+  LatLng? _location;
 
   @override
   void initState() {
@@ -46,14 +54,28 @@ class _ListingDetailsPageState extends State<ListingDetailsPage> {
         debugPrint("Listing Details: $listing");
 
         setState(() {
-          _userName = data['user_name'];
+          _createdBy = data['user_name'];
           _title = listing['title'];
           _description = listing['description'];
+          _category = listing['category'];
+          _formattedDate = formatIsoDate(listing['created_at']);
+          _status = listing['status'];
+          _location = LatLng(
+            double.parse(listing['latitude'].toString()),
+            double.parse(listing['longitude'].toString()),
+          );
         });
       }
     } catch (e) {
       debugPrint("Error: $e");
     }
+  }
+
+  String formatIsoDate(String isoDate) {
+    final dateTime = DateTime.parse(
+      isoDate,
+    ).toLocal(); // convert from UTC if needed
+    return DateFormat('MMM d, y – h:mm a').format(dateTime);
   }
 
   @override
@@ -99,16 +121,18 @@ class _ListingDetailsPageState extends State<ListingDetailsPage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          _userName,
+                          _createdBy,
                           style: const TextStyle(
                             fontWeight: FontWeight.w600,
                             color: Color(0xFF1F2937),
                             fontSize: 16,
                           ),
                         ),
-                        const Text(
-                          'Posted recently',
-                          style: TextStyle(
+
+                        // Created At, formatted
+                        Text(
+                          "Created at: $_formattedDate",
+                          style: const TextStyle(
                             color: Color(0xFF6B7280),
                             fontSize: 14,
                           ),
@@ -121,14 +145,48 @@ class _ListingDetailsPageState extends State<ListingDetailsPage> {
               const SizedBox(height: 32),
 
               // Title
-              Text(
-                _title,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF1F2937),
-                  height: 1.3,
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    _title,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF1F2937),
+                      height: 1.3,
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+
+                  // Current Status
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 4,
+                    ),
+                    decoration: BoxDecoration(
+                      color: _status == Status.active.name
+                          ? const Color(0xFFD1FAE5)
+                          : _status == Status.pickedUp.name
+                          ? const Color(0xFFFEE2E2)
+                          : const Color(0xFFF3F4F6),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Text(
+                      _status.toUpperCase(),
+                      style: TextStyle(
+                        color: _status == Status.booked.name
+                            ? const Color(0xFF065F46)
+                            : _status == Status.pickedUp.name
+                            ? const Color(0xFFB91C1C)
+                            : const Color(0xFF6B7280),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 24),
 
@@ -150,6 +208,21 @@ class _ListingDetailsPageState extends State<ListingDetailsPage> {
                   ),
                 ),
               ),
+              const SizedBox(height: 24),
+
+              // category
+              Text(
+                "Category: $_category",
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Color(0xFF374151),
+                  height: 1.6,
+                ),
+              ),
+              const SizedBox(height: 8),
+
+              // Map to show where the listing is located
+              if (_location != null) ItemLocationMap(location: _location!),
             ],
           ),
         ),
