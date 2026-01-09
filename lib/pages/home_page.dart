0f330af061beab4b782ba4e5827efbea37b3b2d5
Ali Ahmed <eliehmed908@gmail.com>
2026-01-09 12:36:14 +0500

    removed unused code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,8 +15,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // FIREBASE - COMMENTED OUT
-  // final FirestoreService _firestoreService = FirestoreService();
   List<ListingModel> _currentListings = [];
 
   @override
