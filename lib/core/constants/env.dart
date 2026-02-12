4f29d44dbe12c9b2f405d2050e80393e1a1534e1
Ali Ahmed <eliehmed908@gmail.com>
2026-02-12 10:14:28 +0500

    added env.dart

---
+++ lib/core/constants/env.dart
@@ -0,0 +1,6 @@
+class Env {
+  // Map API key for MapTiler
+  static const String mapApiKey = "Ww4nWHUs4BLQV20sy7zB";
+  static const String supabaseUrl = "https://cfiajgroecyvjcrosupq.supabase.co";
+  static const String supabaseAnonKey = "sb_publishable_bU5ITjn3aPPJPFGsXjBG7A_5wf6y_l7";
+}
